6977f226750a9709fd64f4c750927872aca3c8c4
vigneshkannan255 <32855922+vigneshkannan255@users.noreply.github.com>
2020-10-03 13:42:37 +0530

    Update progcode.py

--- progcode.py
+++ progcode.py
@@ -8,15 +8,18 @@ import argparse
 import imutils
 import cv2
 
-
+#function start
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
 ap.add_argument("-w", "--width", type=float, required=True,
 	help="width of the left-most object in the image (in inches)")
 args = vars(ap.parse_args())
+
+#preprocessing
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (7, 7), 0)
